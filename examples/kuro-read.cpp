6360348cb0643f10bc8541c0881677f11b57b8b2
SKTT1Ryze <linuxgnulover@gmail.com>
2022-04-19 22:38:26 +0800

    update example and save

--- examples/kuro-read.cpp
+++ examples/kuro-read.cpp
@@ -11,6 +11,8 @@ Task<int> co_readv(std::shared_ptr<io_uring>& handle) {
   auto open = async_open(handle, "README.md");
   int fd = co_await open;
   
+  // int fd = open("README.md", O_RDONLY);
+  
   if (fd < 0) {
     std::cout << "open file error, fd: " << fd << std::endl;
     co_return -1;
@@ -35,6 +37,8 @@ Task<int> co_read(std::shared_ptr<io_uring>& handle) {
   
   auto open = async_open(handle, "README.md");
   int fd = co_await open;
+
+  // int fd = open("README.md", O_RDONLY);
   
   if (fd < 0) {
     std::cout << "open file error, fd: " << fd << std::endl;
@@ -44,12 +48,8 @@ Task<int> co_read(std::shared_ptr<io_uring>& handle) {
   if(posix_memalign(&buf, BUF_LEN, BUF_LEN))
     co_return 1;
   
-  auto read = Read(handle, fd, buf, BUF_LEN, 0);
-  __s32 _res = co_await read;
-  
-  // auto file = File(fd);
-  // auto read = file.read(handle, buf, BUF_LEN);
-  // __s32 _res = co_await read;
+  auto file = File(fd);
+  co_await file.read(handle, buf, BUF_LEN);
 
   std::cout << "read: " << std::endl << (char*)buf << std::endl;
 
