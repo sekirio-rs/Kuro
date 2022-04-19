a08f4f398b8e0658f043cabc3f7727bd4ac7246a
SKTT1Ryze <linuxgnulover@gmail.com>
2022-04-19 12:27:34 +0800

    remove some comment

--- src/op.cpp
+++ src/op.cpp
@@ -16,11 +16,6 @@ void Op<T>::await_suspend(std::coroutine_handle<> h) {
   cb(sqe);
   io_uring_sqe_set_data(sqe, (void*)token);
 
-  // std::cout << "sqe->opcode: " << sqe->opcode << std::endl;
-  // std::cout << "sqe->fd: " << sqe->fd << std::endl;
-  // std::cout << "sqe->flags: " << sqe->flags << std::endl;
-  // std::cout << "sqe->len: " << sqe->len << std::endl;
-
   CO_HANDLES.insert(std::pair{token, h});
   URING_RESULTS.insert(std::pair{token, &res});
 }
